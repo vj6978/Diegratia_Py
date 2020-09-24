79ee3b2fb1bb8284aadb5c7c73363a30e2809100
Vimal <vj6978@gmail.com>
2020-09-24 23:27:50 +0100

    Mongo

--- utility.py
+++ utility.py
@@ -2,7 +2,10 @@ import pymongo
 import config
 
 class Utility:
-    def saveToMongo(self, tbName, **kwargs):
+    """
+    :parameter tbName table Name to which data should be saved.
+    """
+    def saveToMongo(self, **kwargs):
         client = pymongo.MongoClient(config._URI)
         Database = client.get_database(config._db_string)
-        Database[tbName].insert_one(kwargs)
+        Database[config._tbName].insert_one(kwargs)
